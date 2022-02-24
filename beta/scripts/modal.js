e1feb8337a79db3ba93467cb75511887be3560ad
dylan-roussin <dmr395@drexel.edu>
2022-02-24 15:16:31 -0500

    adjust

--- beta/scripts/modal.js
+++ beta/scripts/modal.js
@@ -1,11 +1,13 @@
 // Get the modal
 const modal = document.getElementById("addcart-modal");
 const modalAcc = document.getElementById("checkout-modal");
+const modalCreate = document.getElementById("create-modal");
 
 
 // Get the button that opens the modal
 const btn = document.getElementById("addcart");
 const checkout = document.getElementById("checkout");
+const create = document.getElementById("create-acc");
 
 
 // When the user clicks on the button, open the modal
@@ -16,6 +18,11 @@ btn.onclick = function() {
 checkout.onclick = function() {
   modalAcc.style.display = "block";
   modal.style.display = "none";
+}
+
+create.onclick = function() {
+  modalAcc.style.display = "none";
+  modalCreate.style.display = "block";
 }
 
 
